b860289b1ed9d084c4a9f3852160c9c09cd8fa84
Austin Cesear <austincesear123@gmail.com>
2022-03-10 10:27:57 -0800

    prep for deployment Co-authored-by: @dribuffo Co-authored-by: @mrb5691

--- connection.js
+++ connection.js
@@ -1,6 +1,13 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1/project-03-api")
+let mongoURL = "";
+
+if (process.env.NODE_ENV === "production") {
+  mongoURL = process.env.DB_URL;
+} else {
+  mongoURL = "mongodb://127.0.0.1/project-03-api";
+}
+
+mongoose.connect(mongoURL);
 
 module.exports = mongoose;
- 
